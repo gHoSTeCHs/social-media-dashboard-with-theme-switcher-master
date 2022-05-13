951602df5ef5b8c25d27a2f9ae21d3337e1d1e2b
gHoSTeCHs <kingswatter@gmail.com>
2022-05-13 08:44:32 +0100

    third commit

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const checkBox = document.getElementById('checkbox');
 const label = document.querySelector('.label')
 const ball = document.querySelector('.ball')
+const follower = document.querySelectorAll('.follower')
+const h1 = document.querySelectorAll('h1')
+const h2 = document.querySelector('h2')
 
 checkBox.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
-  label.classList.toggle('dark')
-  ball.classList.toggle('dark')
+    document.body.classList.toggle('light')
+    label.classList.toggle('light')
+    ball.classList.toggle('light')
+    h2.classList.toggle('light')
+    follower.forEach(follow => {
+        follow.classList.toggle('light')
+    })
+    h1.forEach(h => {
+        h.classList.toggle('light')
+    })
+    
 })
 
 
